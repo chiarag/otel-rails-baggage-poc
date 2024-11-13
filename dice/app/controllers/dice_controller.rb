61411c38d2c046c5eab41a6566dc4e7ce8f7d554
Chiara Gelinas <chiara.gelinas@C.Gelinas-JJ23JT76P3>
2024-11-13 11:23:10 -0800

    initial commit

---
+++ dice/app/controllers/dice_controller.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'opentelemetry-api'
+
+class DiceController < ApplicationController
+  def roll
+    roll = rand(1..6)
+
+    logger.info "traceid #{OpenTelemetry::Trace.current_span.context.trace_id.unpack1('H*')}"
+
+    # in order to propogate baggage, the least janky way I've found is to attach it as when you build it
+    # it returns a brand new context, but does to associate this context with the current or the current entries
+    context_with_baggage = OpenTelemetry::Baggage.build { |b| b.set_value('lost_bag', 'where is it?') }
+    OpenTelemetry::Context.attach(context_with_baggage)
+
+    url = URI.parse("http://localhost:4010/multiply/#{roll}")
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Post.new(url.request_uri)
+
+    response = http.request(request)
+    data = JSON.parse(response.body)
+
+    render json: data
+  end
+end
